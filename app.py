b6e803c1c8ac3a171168d773f7597ac7cff183a5
secarrel <secarrel@outlook.com>
2023-12-28 11:20:08 +0000

    Add: log in functionaility

--- app.py
+++ app.py
@@ -66,13 +66,37 @@ def register():
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
+        flash("Registration Successful! Welcome, {}!".format(request.form.get("username")))
+        return redirect(url_for("my_nest"))
+    
     return render_template("register.html")
 
 
-@app.route("/log_in/", methods=["GET", "POST"])
-def log_in():
-    return render_template("log_in.html")
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}!".format(request.form.get("username")))
+                    return redirect(url_for("my_nest"))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
