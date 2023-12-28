7a74623852fe0ac676f18e2681a326cbd6d51462
secarrel <secarrel@outlook.com>
2023-12-28 16:58:57 +0000

    Add: restrictions to input fields for new observations

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $(".dropdown-trigger").dropdown();
-    $("select").formSelect();
-    $(".datepicker").datepicker();
-    $(".timepicker").timepicker();
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let minOne = new Date(year, month, day - 1);
+let maxDate;
+
+$(document).ready(function () {
+  $(".sidenav").sidenav({ edge: "right" });
+  $(".dropdown-trigger").dropdown();
+  $("select").formSelect();
+  $(".datepicker").datepicker({
+    format: "dd mmm yyyy",
+    yearRange: 1,
+    maxDate: minOne,
+    showClearBtn: true,
   });
+  $(".timepicker").timepicker({
+    showClearBtn: true,
+  });
+});
