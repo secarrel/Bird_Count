04bfe2df5ce79f737dd5a4ff362dbe7b7a60b68d
secarrel <secarrel@outlook.com>
2023-12-28 14:40:11 +0000

    Update: navbars to restrict user pages to the session user

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
-    $("select").formSelect();
     $(".dropdown-trigger").dropdown();
+    $("select").formSelect();
   });
-
