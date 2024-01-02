9c0b441ae57b5ab5a876bfba275ec4a61839e6a6
secarrel <secarrel@outlook.com>
2024-01-02 23:08:41 +0000

    Add: locations for autocomplete in add_observation form

--- static/js/script.js
+++ static/js/script.js
@@ -4,11 +4,19 @@ let month = date.getMonth();
 let day = date.getDate();
 let minOne = new Date(year, month, day - 1);
 let maxDate;
+let locations = {}
+
+
 
 $(document).ready(function () {
+  M.AutoInit();
+
   $(".sidenav").sidenav({ edge: "right" });
+
   $(".dropdown-trigger").dropdown();
+
   $("select").formSelect();
+
   $(".datepicker").datepicker({
     format: "dd mmmm yyyy",
     yearRange: 1,
@@ -16,9 +24,36 @@ $(document).ready(function () {
     showClearBtn: true,
     autoClose: true,
   });
+
   $(".timepicker").timepicker({
     showClearBtn: true,
     autoClose: true,
   });
+
   $(".modal").modal();
+
+  fetch("../static/js/locations.json")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to retrieve autocomplete data");
+      }
+    })
+    .then((locations) => {
+      let autoCompleteData = {};
+      for (let location of locations) {
+        for (let key in location) {
+          autoCompleteData[key] = null;
+        }
+      }
+      $("input.autocomplete").autocomplete({
+        data: autoCompleteData,
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching autocomplete data:", error);
+    });
 });
+
+
